bda52ee622c8d791c2f5a9252858864e0c67cb6c
Takuya Matsuyama <hi@craftz.dog>
2024-09-04 15:00:28 +0900

    feat(toc): add `outlineItemRef` and `isVisible`

--- src/stores/toc.ts
+++ src/stores/toc.ts
@@ -1,19 +1,20 @@
 import { create } from 'zustand'
-import {
-  mdastExtractHeadings,
-  TOCHeading
-} from '@/utils/mdast-extract-headings'
+import { mdastExtractHeadings, TOCHeading } from '@/utils/mdast-toc'
 import { loggerMiddleware } from './logger'
 import type { Root } from 'mdast'
 import type { RefObject } from 'react'
 
 export type Section = TOCHeading & {
   headingRef: RefObject<HTMLHeadingElement> | null
+  outlineItemRef: RefObject<HTMLLIElement> | null
+  isVisible: boolean
 }
 export type TocState = {
   sections: Section[]
   update: (mdast: Root) => void
   registerHeading: (id: string, ref: RefObject<HTMLHeadingElement>) => void
+  registerOutlineItem: (id: string, ref: RefObject<HTMLLIElement>) => void
+  setVisibleHeadings: (ids: string[]) => void
 }
 
 export const useTocStore = create<TocState>(
@@ -26,7 +27,9 @@ export const useTocStore = create<TocState>(
           const prev = prevSections.find(s => s.id === h.id)
           return {
             ...h,
-            headingRef: prev ? prev.headingRef : null
+            isVisible: false,
+            headingRef: prev ? prev.headingRef : null,
+            outlineItemRef: prev ? prev.outlineItemRef : null
           }
         })
 
@@ -40,6 +43,21 @@ export const useTocStore = create<TocState>(
         sections: state.sections.map(s =>
           s.id === id ? { ...s, headingRef: ref } : s
         )
+      }))
+    },
+    registerOutlineItem: (id: string, ref: RefObject<HTMLLIElement>) => {
+      set(state => ({
+        sections: state.sections.map(s =>
+          s.id === id ? { ...s, outlineItemRef: ref } : s
+        )
+      }))
+    },
+    setVisibleHeadings: (ids: string[]) => {
+      set(state => ({
+        sections: state.sections.map(s => ({
+          ...s,
+          isVisible: ids.includes(s.id)
+        }))
       }))
     }
   }))
