2329df307633f51936664c7654647f3428fdf3c1
Takuya Matsuyama <hi@craftz.dog>
2024-09-04 13:44:12 +0900

    fix: use lowercase filenames

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './app.css'
 
 interface CounterState {
   count: number
