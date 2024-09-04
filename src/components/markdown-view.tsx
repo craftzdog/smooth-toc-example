82e7f0b010b598c1803fd758f2d255a10c1b5567
Takuya Matsuyama <hi@craftz.dog>
2024-09-04 14:16:41 +0900

    feat(ui): render a markdown content

--- src/components/markdown-view.tsx
+++ src/components/markdown-view.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Box } from '@kuma-ui/core'
 import markdownContent from '@/example.md?raw'
+import { useContentStore } from '@/stores/content'
 
 export const MarkdownView: React.FC = () => {
-  return <Box className="markdown-view">{markdownContent}</Box>
+  const { dom, render } = useContentStore()
+
+  useEffect(() => {
+    render(markdownContent)
+  }, [render])
+
+  return <Box className="markdown-view">{dom ? dom : 'rendering...'}</Box>
 }
