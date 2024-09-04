15367a8ce67ae47ce7d66d2d6572b6df56c9a90e
Takuya Matsuyama <hi@craftz.dog>
2024-09-04 13:22:49 +0900

    style(prettier): format

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
