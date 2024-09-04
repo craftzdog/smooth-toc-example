8736d99a4328d42b4f3e4054557fa4d66b282be8
Takuya Matsuyama <hi@craftz.dog>
2024-09-04 13:37:24 +0900

    chore(eslint): configure

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,15 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        }
+      ],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true }
