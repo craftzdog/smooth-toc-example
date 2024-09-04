2fb4a93fdb32b306232b9da23173a92ca6401599
Takuya Matsuyama <hi@craftz.dog>
2024-09-04 13:39:02 +0900

    build(vite): configure

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from 'url'
 import KumaUI from '@kuma-ui/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
@@ -9,5 +10,13 @@ export default defineConfig({
     KumaUI({
       wasm: true
     })
-  ]
+  ],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      }
+    ]
+  }
 })
