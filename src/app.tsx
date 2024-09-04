ff00e230a4fb9ea259b61e1ab6f3688160ef4d2a
Takuya Matsuyama <hi@craftz.dog>
2024-09-04 14:14:05 +0900

    chore(app): clean up

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,9 @@
-import { create } from 'zustand'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './app.css'
 import { Container } from './atoms/container'
 import { MainContent } from './components/main-content'
 import { MarkdownView } from './components/markdown-view'
 
-interface CounterState {
-  count: number
-  increment: () => void
-}
-
-const useCounterStore = create<CounterState>(set => ({
-  count: 0,
-  increment: () => set(state => ({ count: state.count + 1 }))
-}))
-
 function App() {
-  const { count, increment } = useCounterStore()
-
   return (
     <Container>
       <MainContent>
